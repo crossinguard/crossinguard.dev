8b83ca6f381ef13490eaad25a3a57be95567d666
Brett Egbert <brett@crossinguard.dev>
2024-04-19 14:51:22 -0700

    local svgs component; split css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,10 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crossinguard.dev',
-  integrations: [mdx(), sitemap()],
+  integrations: [mdx(), sitemap(), icon()]
 });
